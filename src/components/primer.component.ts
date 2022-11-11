cb510c7ceb4ad616c631876f6512e402ddf3a7c1
sergioalcazar745 <sergioalcazar745@gmail.com>
2022-11-11 09:40:56 +0100

    first commit

---
+++ src/components/primer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'primer-component',
+    template: `
+        <h1>Soy el primer component de Angular</h1>
+    `
+})
+
+export class PrimerComponent{
+
+}
